ce28104d79a7d33a0644d48399fe38f65831c848
korede <korede@yahoo.com>
2019-07-16 10:41:55 +0100

    finished view users/started edit feature

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -9,5 +9,25 @@ class UsersController extends AppController
     {
         $this->set('users',$this->Users->find('all'));
     }
+
+    public function add()
+    {
+        $post = $this->Users->newEntity();
+            if($this->request->is('post')){
+                $post = $this->Users->patchEntity($post, $this->request->getData());
+                if($this->Users->save($post)){
+                    $this->Flash->success('Post Added successfully', ['key'=>'message']);
+                    return $this->redirect(['action'=>'index']);
+                }
+                $this->Flash->error(__("unable to add your post"));
+            }
+            $this->set('users', $post);
+    }
+
+    public function view($id= Null){
+        // code...
+            $post = $this->Users->get(;)
+    }
+
 }
 ?>
